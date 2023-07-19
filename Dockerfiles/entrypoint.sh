22265c09dc6d10c33cf77b6649ebb0dfe29d5db3
Florian Fischer <florian.fischer@muhq.space>
2023-07-19 11:35:29 +0200

    support building single file bundles

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -19,6 +19,11 @@ SPEC_FILE=${4:-*.spec}
 
 REQUIREMENTS=${5:-requirements.txt}
 
+SINGLE_FILE_ARG=""
+if [ "${6:-False}" = "True" ]; then
+    SINGLE_FILE_ARG="-F"
+fi
+
 python -m pip install --upgrade pip wheel setuptools
 
 #
@@ -27,29 +32,31 @@ python -m pip install --upgrade pip wheel setuptools
 #
 if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
    [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
-    # the funky looking regexp just extracts the hostname, excluding port
-    # to be used as a trusted-host.
     mkdir -p /wine/drive_c/users/root/pip
-    echo "[global]" > /wine/drive_c/users/root/pip/pip.ini
-    echo "index = $PYPI_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "index-url = $PYPI_INDEX_URL" >> /wine/drive_c/users/root/pip/pip.ini
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /wine/drive_c/users/root/pip/pip.ini
+    {
+        echo "[global]"
+        echo "index = $PYPI_URL"
+        echo "index-url = $PYPI_INDEX_URL"
+        # the funky looking regexp just extracts the hostname, excluding port
+        # to be used as a trusted-host.
+        echo "trusted-host = $(echo "$PYPI_URL" | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')"
+    } > /wine/drive_c/users/root/pip/pip.ini
 
     echo "Using custom pip.ini: "
     cat /wine/drive_c/users/root/pip/pip.ini
 fi
 
-cd $WORKDIR
+cd "$WORKDIR"
 
-if [ -f ${REQUIREMENTS} ]; then
-    pip install -r ${REQUIREMENTS}
+if [ -f "${REQUIREMENTS}" ]; then
+    pip install -r "${REQUIREMENTS}"
 fi
 
 
 
-if [[ "$@" == "" ]]; then
+if [[ "$*" == "" ]]; then
     bash
 else
-    pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
+    pyinstaller ${SINGLE_FILE_ARG} --clean -y --dist ./dist/windows --workpath /tmp "${SPEC_FILE}"
     chown -R --reference=. ./dist/windows
 fi
